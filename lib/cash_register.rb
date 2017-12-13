6e7712c8efcb68d4c62c3010d72ebd720056486a
James Ferguson <ncjamesferguson@gmail.com>
2017-12-13 07:22:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   end
 
   def apply_discount
-      if self.discount == 0
+      if @discount == 0
         puts "There is no discount to apply."
       else
         self.total = (total * ((100.0 - discount.to_f)/100)).to_i
